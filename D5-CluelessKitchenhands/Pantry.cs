baaba6aa149501b2ddc3b2f7c7f732b6db844084
George <george@streamliners.co.nz>
2025-12-06 13:05:12 +1300

    Done part 1

--- D5-CluelessKitchenhands/Pantry.cs
+++ D5-CluelessKitchenhands/Pantry.cs
@@ -1,25 +1,51 @@
 
 public class Pantry
 {
-    public struct FreshRange (int IdMin, int IdMax)
+    public struct FreshRange (long IdMin, long IdMax)
     {
-        public bool IsIdInRange (int id)
+        public bool IsIdInRange(long id)
         {
-            return id > IdMin && id < IdMax;
+            return id >= IdMin && id <= IdMax;
+        }
+        
+        public static FreshRange FromString (string data)
+        {
+            var numericParts = data.Split('-').Select(long.Parse).ToArray();
+            return new FreshRange(numericParts[0], numericParts[1]);
         }
     }
 
     readonly IEnumerable<FreshRange> freshRanges;
+    readonly IEnumerable<long> ingredientIds;
 
     public Pantry (string data)
     {
-        
+        IEnumerable<string> lines = data.Split('\n')
+            .Select(str => str.Trim())
+            .Where(str => !string.IsNullOrEmpty(str));
 
+        IEnumerable<string> rangeData = lines.Where(line => line.Contains('-'));
+        IEnumerable<string> ingredientIdData = lines.Where(line => !line.Contains('-'));
+
+        freshRanges = rangeData.Select(FreshRange.FromString);
+        ingredientIds = ingredientIdData.Select(long.Parse);
+        Console.WriteLine();
     }
 
-    public static Pantry FromFile (string path)
+    public static Pantry FromFile(string path)
     {
-        File.ReadAllLines(path);
-        
+        return new Pantry(File.ReadAllText(path));
+    }
+    
+    public IEnumerable<long> GetFreshIngredientIds()
+    {
+        return ingredientIds.Where(id =>
+        {
+            foreach (FreshRange range in freshRanges)
+            {
+                if (range.IsIdInRange(id)) return true;
+            }
+            return false;
+        });
     }
 }
--- D5-CluelessKitchenhands/Program.cs
+++ D5-CluelessKitchenhands/Program.cs
@@ -4,12 +4,31 @@ public static class Program
 {
     public static void PerformTests()
     {
+        string testData = @"3-5
+10-14
+16-20
+12-18
 
+1
+5
+8
+11
+17
+32";
+
+        Pantry pantry = new Pantry(testData);
+        IEnumerable<long> freshIngredientIds = pantry.GetFreshIngredientIds();
+
+        Console.WriteLine($"There are {freshIngredientIds.Count()} fresh ingredients in the pantry");
     }
 
     public static void PerformPuzzleOne()
     {
 
+        Pantry pantry = Pantry.FromFile(@".\input.txt");
+        IEnumerable<long> freshIngredientIds = pantry.GetFreshIngredientIds();
+
+        Console.WriteLine($"There are {freshIngredientIds.Count()} fresh ingredients in the pantry");
     }
     
     public static void PerformPuzzleTwo()
@@ -19,6 +38,6 @@ public static class Program
 
     public static void Main (string[] args)
     {
-        
+        PerformPuzzleOne();
     }
 }
