96119deb4917a286ab133fe8baa6533e2c6fc9e6
George Anderson <george@streamliners.co.nz>
2025-12-08 13:56:18 +1300

    Where we're going we don't need wheels

--- D7-TachyTech/Program.cs
+++ D7-TachyTech/Program.cs
@@ -21,21 +21,32 @@ public static class Program
 .^.^.^.^.^...^.
 ...............";
 
-        
+        TachyonManifold manifold = new(testData);
+        TachyonManifold.TachyonSplittingResult splittingResult = manifold.RunTachyonSplittingManifold();
+
+        Console.WriteLine($"The tachyon manifold split a beam {splittingResult.tachyonPathCount} times");
     }
 
     static void PerformPuzzleOne()
     {
+        TachyonManifold manifold = TachyonManifold.FromFile(@".\input.txt");
+        TachyonManifold.TachyonSplittingResult splittingResult = manifold.RunTachyonSplittingManifold();
 
+        Console.WriteLine($"The tachyon manifold split a beam {splittingResult.splitterActivationCount} times");
     }
     
     static void PerformPuzzleTwo()
     {
-        
+        TachyonManifold manifold = TachyonManifold.FromFile(@".\input.txt");
+        TachyonManifold.TachyonSplittingResult splittingResult = manifold.RunTachyonSplittingManifold();
+
+        Console.WriteLine($"The tachyon manifold split a beam into {splittingResult.tachyonPathCount} probable paths");
     }
 
     public static void Main(string[] args)
     {
-        
+        // PerformTests();
+        // PerformPuzzleOne();
+        PerformPuzzleTwo();
     }
 }
--- D7-TachyTech/TachyonManifold.cs
+++ D7-TachyTech/TachyonManifold.cs
@@ -1,24 +1,106 @@
+using System.Net;
+
 public class TachyonManifold
 {
-    public struct Splitter
+    public struct TachyonSplittingResult
+    {
+        public int splitterActivationCount;
+        public long tachyonPathCount;
+    }
+    
+    struct Splitter
     {
         public int location;
     }
+
+    class Beam
+    {
+        public long possibleAncestralPaths = 1;
+    }
     
-    HashSet<int> Beams = [];
-    Dictionary<int, Splitter> SplitterBanks = [];
+    Dictionary<int, Beam> PositionedBeams = [];
+    Dictionary<int, IEnumerable<Splitter>> SplitterBanks = [];
+    readonly int BeamEntryPoint;
+    readonly int TotalRanks;
 
     public TachyonManifold(string data)
     {
-        IEnumerable<string> lines = data
+        string[] lines = data
             .Split('\n')
             .Select(str => str.Trim())
-            .Where(str => !string.IsNullOrWhiteSpace(str));
+            .Where(str => !string.IsNullOrWhiteSpace(str))
+            .ToArray();
         
-        foreach (string line in lines)
+        // interate through ranks
+        for (int i=0; i<lines.Count(); i++)
         {
-            
+            string line = lines[i];
+            // beam emission is on the first rank
+            if (i == 0) 
+            {
+                BeamEntryPoint = line.IndexOf('S');
+                continue;
+            }
+
+            List<Splitter> splitters = [];
+            // find splitters
+            for (int c=0; c<lines[0].Length; c++)
+            {
+                if (line[c] == '^') 
+                    splitters.Add(new Splitter {location=c});
+            }
+
+            if (splitters.Count > 0) SplitterBanks.Add(i, splitters);
+            TotalRanks = i;
         }
+    }
+
+    void SpawnBeam (int position, long currentAncestralPaths)
+    {
+        if (!PositionedBeams.TryAdd(position, new Beam() {possibleAncestralPaths = currentAncestralPaths}))
+        {
+            // Beam exists
+            PositionedBeams[position].possibleAncestralPaths += currentAncestralPaths;
+        }
+    }
+
+    public TachyonSplittingResult RunTachyonSplittingManifold ()
+    {
+        PositionedBeams.Add(BeamEntryPoint, new Beam ());
+
+        int splitterActivationCount = 0;
+        // iterate through splitter banks adding and removing beams on encountering splitter
+        for (int rank=1; rank<TotalRanks; rank++)
+        {
+            // no splitters mave on
+            if (!SplitterBanks.ContainsKey(rank)) continue;
+            foreach (Splitter splitter in SplitterBanks[rank])
+            {
+                if (PositionedBeams.ContainsKey(splitter.location))
+                {
+                    // split beam
+                    long currentAncestralPaths = PositionedBeams[splitter.location].possibleAncestralPaths;
+                    
+                    PositionedBeams.Remove(splitter.location);
+                    
+                    SpawnBeam(splitter.location+1, currentAncestralPaths);
+                    SpawnBeam(splitter.location-1, currentAncestralPaths);
+
+                    splitterActivationCount++;
+                }
+            }
+        }
+
+        long tachyonPathCount = PositionedBeams.Select(kvp => kvp.Value.possibleAncestralPaths).Sum();
+        
+        // beams dissipate
+        PositionedBeams = [];
+
+        return new TachyonSplittingResult
+        {
+            splitterActivationCount = splitterActivationCount,
+            tachyonPathCount = tachyonPathCount
+        };
     }
     
     public static TachyonManifold FromFile (string path)
